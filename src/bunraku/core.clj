898fe2c307538c9e4ca0e5ca0ce8c395b7122961
bipol-cb <bipol.alam@careerbuilder.com>
2017-03-07 21:46:08 -0500

    Add more selenium commands. (#2)
    * Add another func
    
    * fix checkouts folder
    
    * oops
    
    * Add html unit driver

--- src/bunraku/core.clj
+++ src/bunraku/core.clj
@@ -1,10 +1,16 @@
 (ns bunraku.core
   (:import [org.openqa.selenium.chrome ChromeDriver]
+           [org.openqa.selenium.htmlunit HtmlUnitDriver]
            [org.openqa.selenium By]))
 
 (defn chrome-driver
   []
   {:driver (new ChromeDriver)
+   :element nil})
+
+(defn html-driver
+  []
+  {:driver (new HtmlUnitDriver)
    :element nil})
 
 (defn get-url
@@ -31,6 +37,10 @@
   [{:keys [driver element]}]
   (Thread/sleep 1500)
   (.getTitle driver))
+
+(defn get-current-url
+  [{:keys [driver element]}]
+  (.getCurrentUrl driver))
 
 (defn click
   [{:keys [driver element] :as original}]
